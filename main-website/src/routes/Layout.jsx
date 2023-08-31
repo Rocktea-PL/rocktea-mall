b8975e88e7bedb60c8319fab9fa5307056ff83c5
yeeshadev <aishaagunbiade05@gmail.com>
2023-08-31 11:28:37 +0100

    added the layout of the website

---
+++ main-website/src/routes/Layout.jsx
@@ -0,0 +1,22 @@
+import { Outlet, useLocation } from 'react-router-dom'
+import Navbar from '../Features/Navbar';
+import Footer from '../Features/Footer';
+
+
+function Layout() {
+  const location = useLocation();
+
+  // Check if the current location is the registration route
+  const hideNavbar = location.pathname === '/register' || location.pathname === '/login';
+
+  return (
+    <main className='relative'>
+      
+      {!hideNavbar && <Navbar />} 
+        <Outlet/>
+        {!hideNavbar && <Footer />}
+    </main>
+  )
+}
+
+export default Layout
