b91bb3c3126a4824376ffbf02ec972d6e2264930
yeeshadev <aishaagunbiade05@gmail.com>
2023-10-20 13:54:22 +0100

    split pages for dropshippers and users

--- main-website/pages/DropShippers/Dashboard.jsx
+++ main-website/pages/DropShippers/Dashboard.jsx
@@ -1,15 +1,14 @@
 //import { useEffect } from "react";
 import { useEffect } from "react";
-import Chart from "../../../product-website/src/components/Dashboard/Chart";
-import OrderStatus from "../../../product-website/src/components/Dashboard/OrderStatus";
-import ProductInStock from "../../../product-website/src/components/Dashboard/ProductInStock";
-//import Sidebars from "../../../product-website/src/components/Dashboard/Sidebars";
-import TopCards from "../../../product-website/src/components/Dashboard/TopCards";
+import TopCards from '../../src/Dropshippers/components/Dashboard/TopCards'
 import Footer from "../../src/Dropshippers/Features/Footer";
 import Navbar from "../../src/Dropshippers/Features/Navbar";
 //import Navbar from "../../src/Dropshippers/Features/UserNavbar";
 import { useState } from "react";
 import Sidebars from "../../src/Dropshippers/components/Dashboard/Sidebars";
+import BarChart from "../../src/Dropshippers/components/Dashboard/Chart";
+import OrderStatus from "../../src/Dropshippers/components/Dashboard/OrderStatus";
+import ProductInStock from "../../src/Dropshippers/components/Dashboard/ProductInStock";
 
 
 function Dashboard() {
@@ -33,7 +32,7 @@ function Dashboard() {
           <TopCards />
           <div className="flex flex-col lg:flex-row justify-center lg:justify-between w-full">
             <Sidebars />
-            <Chart />
+            <BarChart />
           </div>
           <OrderStatus />
           <ProductInStock />
