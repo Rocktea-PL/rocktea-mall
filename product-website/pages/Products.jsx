daa1b1922917ff4249e86d973ecacfb989b19b40
yeeshadev <aishaagunbiade05@gmail.com>
2023-10-20 13:41:40 +0100

    split pages for dropshippers and users

--- product-website/pages/Products.jsx
+++ product-website/pages/Products.jsx
@@ -1,13 +1,13 @@
 import ProductFilter from "../src/components/Products/FilterCards";
 import Hero from "../src/components/Products/Hero";
-import AllProducts from "../src/components/Products/AllProducts";
+//import AllProducts from "../src/components/Products/AllProducts";
 
 export default function Products() {
   return (
     <main className="mt-[5rem] mx-auto max-w-[1250px] ">
       <Hero />
       <ProductFilter />
-      <AllProducts />
+     
     </main>
   );
 }
