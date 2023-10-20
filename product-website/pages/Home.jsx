d60c4bbb9d1b5ba9a82dcfe410589f8e8ad49654
yeeshadev <aishaagunbiade05@gmail.com>
2023-10-20 13:13:44 +0100

    split pages for dropshippers and users

--- product-website/pages/Home.jsx
+++ product-website/pages/Home.jsx
@@ -2,7 +2,7 @@ import Footer from "../src/Features/Footer";
 import Navbar from "../src/Features/UserNavbar";
 import Categories from "../src/components/Home/Categories";
 import Hero from "../src/components/Home/Hero";
-import Partners from "../src/components/Home/Partners";
+
 import Products from "../src/components/Home/Products";
 //import ImageScroll from "../src/components/Products/categoryScroll";
 
@@ -15,7 +15,7 @@ export default function Home() {
       <Hero />
       <Categories />
       <Products />
-      <Partners />
+      
     </div>
     <Footer />
     </>
