de5d8f9a9badf9428bd77c207d8fba1257d7edf9
yeeshadev <aishaagunbiade05@gmail.com>
2023-10-20 13:24:58 +0100

    split pages for dropshippers and users

--- main-website/pages/DropShippers/Home.jsx
+++ main-website/pages/DropShippers/Home.jsx
@@ -1,9 +1,10 @@
 import Categories from "../../../product-website/src/components/Home/Categories";
 import Hero from "../../../product-website/src/components/Home/Hero";
-import Partners from "../../../product-website/src/components/Home/Partners";
+//import Partners from "../../../product-website/src/components/Home/Partners";
 import Products from "../../../product-website/src/components/Home/Products";
 import Footer from "../../src/Dropshippers/Features/Footer";
 import Navbar from "../../src/Dropshippers/Features/Navbar";
+
 ////import UserNavbar from "../../src/Dropshippers/Features/UserNavbar";
 //import { useAuthContext } from "../../src/hooks/AuthContext";
 //import ImageScroll from "../src/components/Products/categoryScroll";
@@ -17,7 +18,7 @@ export default function HomeStore() {
       <Hero />
       <Categories />
       <Products />
-      <Partners />
+     
     </div>
     <Footer />
     </>
