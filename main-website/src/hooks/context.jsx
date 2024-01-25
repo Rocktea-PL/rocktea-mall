5db7e0a1ad7001958e6fa886ad8bed95aa218852
yeeshadev <aishaagunbiade05@gmail.com>
2024-01-25 11:32:30 +0100

    dropshippers

--- main-website/src/hooks/context.jsx
+++ main-website/src/hooks/context.jsx
@@ -292,7 +292,7 @@ const AppProvider = ({ children }) => {
         response.user_data.has_store
       ) {
         window.open(
-          `http://localhost:5174/dashboard/home?store_id=${response.user_data.store_id}`,
+          `https://rocktea-mall-product.vercel.app/dashboard/home?store_id=${response.user_data.store_id}`,
           "_self",
         );
       } else {
